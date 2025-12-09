6b20af06861ac8e7d099a8bd9452f01c6adbc6c2
Ana Seseña <aesesrm@gmail.com>
2025-12-09 15:21:34 +0100

    clase granHacker

--- app/src/main/java/com/ldm/elGranHacker/juego/JuegoGranHacker.java
+++ app/src/main/java/com/ldm/elGranHacker/juego/JuegoGranHacker.java
@@ -3,7 +3,7 @@ package com.ldm.elGranHacker.juego;
 import com.ldm.elGranHacker.Pantalla;
 import com.ldm.elGranHacker.androidimpl.AndroidJuego;
 
-public class JuegoChefMaestro extends AndroidJuego {
+public class JuegoGranHacker extends AndroidJuego {
     @Override
     public Pantalla getStartScreen() {
         return new LoadingScreen(this);
